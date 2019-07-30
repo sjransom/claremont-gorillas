6adb2d6e18b3db665c649832ecebd20ab81f8921
sjransom <sjransom@gmail.com>
2019-07-30 17:24:49 +0100

    break out json into different file

--- src/App.js
+++ src/App.js
@@ -1,46 +1,13 @@
 import React from 'react';
 import 'normalize.css/normalize.css'
 import './styles/styles.scss';
+
+import PlayerData from './data/playerData'
 import Header from './components/Header'
 import Players from './components/Players'
 
 class App extends React.Component {
-  state = {
-    players: {
-      player1: {
-        name: "Sam Ransom",
-        image: "/images/footballer.jpg",
-        age: 29,
-        position: "Right back",
-        appearences: 9,
-        goals: 0
-      },
-      player2: {
-        name: "Adam Burns",
-        image: "/images/footballer.jpg",
-        age: 32,
-        position: "Attacking mildfielder",
-        appearences: 11,
-        goals: 2
-      },
-      player3: {
-        name: "Matt Clark",
-        image: "/images/footballer.jpg",
-        age: 28,
-        position: "Central midfielder",
-        appearences: 19,
-        goals: 6
-      },
-      player4: {
-        name: "Ollie Smith",
-        image: "/images/footballer.jpg",
-        age: 28,
-        position: "Goalkeeper",
-        appearences: 15,
-        goals: 0
-      }
-    }
-  }
+  state = {}
 
   render() {
     return (
@@ -49,10 +16,10 @@ class App extends React.Component {
         <section className="container">
           <h3 className="page-title">Players</h3>
           <div className="player-list">
-            {Object.keys(this.state.players).map(key => (
+            {Object.keys(PlayerData).map(key => (
               <Players
                 key={key}
-                details={this.state.players[key]}
+                details={PlayerData[key]}
               />
             ))}
           </div>
