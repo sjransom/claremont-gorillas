35f34d4d57011450f934bcae2ca709aa45afa474
sjransom <sjransom@gmail.com>
2020-06-30 17:18:04 +0100

    clean up

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,42 +1,19 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/use-static-query/
- */
-
 import React from 'react'
-import PropTypes from 'prop-types'
-import { useStaticQuery, graphql } from 'gatsby'
 
 import Header from './header'
 import './layout.css'
 import layoutStyles from './layout.module.css'
 
 const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `)
-
   return (
     <>
-      <Header siteTitle={data.site.siteMetadata.title} />
-      <div className={layoutStyles.layout}>
+      <Header />
+      <div className={layoutStyles.main}>
         <main>{children}</main>
         <footer></footer>
       </div>
     </>
   )
-}
-
-Layout.propTypes = {
-  children: PropTypes.node.isRequired,
 }
 
 export default Layout
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,7 @@
-import React from "react"
+import React from 'react'
 
-import Layout from "../components/layout"
-// import Image from "../components/image"
-import SEO from "../components/seo"
+import Layout from '../components/layout'
+import SEO from '../components/seo'
 
 const IndexPage = () => (
   <Layout>
