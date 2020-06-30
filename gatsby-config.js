6e9daed098ba988b1f4bcfd4047f029558b41698
sjransom <sjransom@gmail.com>
2020-06-30 13:36:13 +0100

    full refresh

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `London Gorillas`,
+    description: `London Gorillas are a football team who play in the Wimbledon & District League`,
+    author: `@sjransom`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
