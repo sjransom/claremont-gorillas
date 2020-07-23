6796a2467c6c87665ac43cddf173e94b04268198
sjransom <sjransom@gmail.com>
2020-07-23 15:49:37 +0100

    add nav bar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,13 @@
 import React from 'react'
+
+import NavBar from './nav-bar'
 import headerStyles from './header.module.css'
 
 const Header = () => (
   <header className={headerStyles.header}>
-    <div className={headerStyles.headerContent}></div>
+    <div className={headerStyles.headerContent}>
+      <NavBar />
+    </div>
   </header>
 )
 
---
+++ src/components/nav-bar.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+import { Link } from 'gatsby'
+import navbarStyles from './nav-bar.module.css'
+
+const NavBar = () => {
+  return (
+    <>
+      <nav>
+        <ul className={navbarStyles.navBar}>
+          <li>
+            <Link to="/blog">Home</Link>
+          </li>
+          <li>
+            <Link to="/players">Players</Link>
+          </li>
+          <li>
+            <Link to="/fixtures">Fixtures</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+        </ul>
+      </nav>
+    </>
+  )
+}
+
+export default NavBar
